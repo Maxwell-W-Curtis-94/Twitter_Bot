baac3cf4fe37b596b886ba5b7d548a13d126caf2
Maxwell Curtis <57647617+Maxwell-W-Curtis-94@users.noreply.github.com>
2020-12-09 14:14:58 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="example-pkg-testing-pip-import",  # Replace with your own username
+    version="0.0.1",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+)
